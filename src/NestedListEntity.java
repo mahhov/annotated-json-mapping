055d40bd2eda711e34eef86adb04a2d44ba0e95e
m <mahhov1@gmail.com>
2017-11-15 08:59:38 -0800

    transposed lists

--- src/NestedListEntity.java
+++ src/NestedListEntity.java
@@ -6,4 +6,12 @@ public class NestedListEntity {
     static class NestedList {
         ArrayList<Integer> innerList;
     }
+
+    @JsonAnnotation("")
+    ArrayList<TransposedNestedList> transposedNestedList;
+
+    static class TransposedNestedList {
+        @JsonAnnotation("nestedList.1/innerList.0")
+        ArrayList<Integer> innerList;
+    }
 }
