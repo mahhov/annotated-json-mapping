afe0e6b88ea4abf3dec5f04253bd34de417e9451
m <mahhov1@gmail.com>
2017-11-16 08:38:27 -0800

    unit tests for ArrayGrower

---
+++ test/utility/ArrayGrowerTest.java
@@ -0,0 +1,23 @@
+package utility;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+class ArrayGrowerTest {
+    @Test
+    void append() {
+        int[] orig = new int[] {6, 1, 3, 4};
+        int[] copy = ArrayGrower.append(orig, 10);
+        assertArrayEquals(orig, new int[] {6, 1, 3, 4});
+        assertArrayEquals(copy, new int[] {6, 1, 3, 4, 10});
+    }
+
+    @Test
+    void set() {
+        int[] orig = new int[] {6, 1, 3, 4};
+        int[] copy = ArrayGrower.set(orig, 1, 10);
+        assertArrayEquals(orig, new int[] {6, 1, 3, 4});
+        assertArrayEquals(copy, new int[] {6, 10, 3, 4});
+    }
+}
